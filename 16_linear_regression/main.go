5c44a3037fd50bb31c48286e218ac0304818b76c
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-09-29 09:55:40 +0100

    Train-test split

--- 16_linear_regression/main.go
+++ 16_linear_regression/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -23,17 +24,119 @@ func main() {
 	df := dataframe.ReadCSV(f)
 
 	// Describe not yet available
-	describe(df)
+	describeData(df)
 
 	if err := createHistograms(df); err != nil {
 		return
 	}
 
+	if err := creatScatterplots(df); err != nil {
+		return
+	}
+
+	if err := trainTestSplit(df); err != nil {
+		return
+	}
+
+	fmt.Println("Done!")
 }
 
-Pfunc describe(df dataframe.DataFrame) {
+func describeData(df dataframe.DataFrame) {
 	// 	summary := df.Describe()
 	// 	fmt.Println(summary)
+}
+
+// Do 80-20 split
+func trainTestSplit(df dataframe.DataFrame) error {
+	// Calculate number of elements in each set
+	trainNum := (4 * df.Nrow()) / 5
+	testNum := df.Nrow() / 5
+
+	if trainNum+testNum < df.Nrow() {
+		trainNum++
+	}
+
+	// Create subset indices
+	trainIdx := make([]int, trainNum)
+	testIdx := make([]int, testNum)
+
+	// Enumerate indices
+	for i := 0; i < trainNum; i++ {
+		trainIdx[i] = i
+	}
+
+	for i := 0; i < testNum; i++ {
+		testIdx[i] = i
+	}
+
+	traindDF := df.Subset(trainIdx)
+	testDF := df.Subset(testIdx)
+
+	// Create map that will be used in writing the data to files
+	setMap := map[int]dataframe.DataFrame{
+		0: traindDF,
+		1: testDF,
+	}
+
+	// Create files
+	for idx, setName := range []string{"training.csv", "test.csv"} {
+		f, err := os.Create(setName)
+		if err != nil {
+			log.Printf("Error creating CSV file %s\n", err.Error())
+			return err
+		}
+
+		w := bufio.NewWriter(f)
+		if err := setMap[idx].WriteCSV(w); err != nil {
+			log.Printf("Error writing to CSV file %s\n", err.Error())
+			return err
+		}
+	}
+
+	log.Println("Train-test split done")
+	return nil
+}
+
+// Scatterplot is a way of identifying the dependant variables
+func creatScatterplots(df dataframe.DataFrame) error {
+	// Ectract the target column
+	yVals := df.Col("Sales").Float()
+	for _, name := range df.Names() {
+		// pts holds data to be plotted
+		pts := make(plotter.XYs, df.Nrow())
+		for i, fVal := range df.Col(name).Float() {
+			pts[i].X = fVal
+			pts[i].Y = yVals[i]
+		}
+
+		// Create plot
+		p, err := plot.New()
+		if err != nil {
+			log.Printf("Error plotting %s %s\n", name, err.Error())
+			return err
+		}
+
+		p.X.Label.Text = name
+		p.Y.Label.Text = "y"
+		p.Add(plotter.NewGrid())
+
+		s, err := plotter.NewScatter(pts)
+		if err != nil {
+			log.Printf("Error creating new scatter plot for %s %s\n", name, err.Error())
+			return err
+		}
+
+		s.GlyphStyle.Radius = vg.Points(3)
+		p.Add(s)
+		if err := p.Save(4*vg.Inch, 4*vg.Inch, name+"_scatter.png"); err != nil {
+			log.Printf("Error saving scatter plot %s\n", err.Error())
+			return err
+		}
+	}
+
+	log.Println("Done creating scatter plots")
+	// Both Radio and TV have a somewhat linear relationship with sales
+	return nil
 }
 
 // Create histogram for each column in the data set
@@ -57,19 +160,19 @@ func createHistograms(df dataframe.DataFrame) error {
 		// Create histogram
 		h, err := plotter.NewHist(plotVals, 16)
 		if err != nil {
-			fmt.Printf("Error plotting %s %s\n", name, err.Error())
+			log.Printf("Error plotting %s %s\n", name, err.Error())
 			return err
 		}
 		h.Normalize(1)
 
 		// Add histogram to plot
 		p.Add(h)
-		if err := p.Save(4*vg.Inch, 4*vg.Inch, name+".png"); err != nil {
-			fmt.Printf("Error saving plot %s\n", err.Error())
+		if err := p.Save(4*vg.Inch, 4*vg.Inch, name+"_histogram.png"); err != nil {
+			log.Printf("Error saving plot %s\n", err.Error())
 			return err
 		}
 	}
 
-	fmt.Println("Done creating histograms")
+	log.Println("Done creating histograms")
 	return nil
 }
