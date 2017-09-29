f7d9ac056cf593073df904caaae46eb5c6a8e601
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-09-29 10:09:22 +0100

    Create training model

--- 16_linear_regression/main.go
+++ 16_linear_regression/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
+	"github.com/kniren/gota/dataframe"
+	"github.com/sajari/regression"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
-
-	"github.com/kniren/gota/dataframe"
 )
 
 func main() {
@@ -38,7 +40,59 @@ func main() {
 		return
 	}
 
+	if err := trainModel(); err != nil {
+		return
+	}
+
 	fmt.Println("Done!")
+}
+
+func trainModel() error {
+	f, err := os.Open("training.csv")
+	if err != nil {
+		fmt.Printf("Error opening training file %s\n", err.Error())
+		return err
+	}
+	defer f.Close()
+
+	// Read all training data
+	reader := csv.NewReader(f)
+	reader.FieldsPerRecord = 4
+	trainingData, err := reader.ReadAll()
+	if err != nil {
+		log.Printf("Error reading training data %s\n", err.Error())
+		return err
+	}
+
+	var r regression.Regression
+	r.SetObserved("Sales")
+	r.SetVar(0, "TV")
+	for i, record := range trainingData {
+		// Skip header
+		if i == 0 {
+			continue
+		}
+
+		// Parse sales val
+		yVal, err := strconv.ParseFloat(record[3], 64)
+		if err != nil {
+			log.Printf("Error parsing sales data %s\n", err.Error())
+			return err
+		}
+
+		// Parse TV val
+		tvVal, err := strconv.ParseFloat(record[0], 64)
+		if err != nil {
+			log.Printf("Error parsing sales data %s\n", err.Error())
+			return err
+		}
+
+		// Add data ponts to training model and do the training
+		r.Train(regression.DataPoint(yVal, []float64{tvVal}))
+		r.Run()
+	}
+	fmt.Printf("\nRegression formula: \n%v\n", r.Formula)
+	return nil
 }
 
 func describeData(df dataframe.DataFrame) {
